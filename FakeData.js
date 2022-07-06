a94480c4540580648e5f7c5bd1b395cef95abca2
toremann <50668920+toremann@users.noreply.github.com>
2022-07-06 23:16:22 +0200

    apollo-graphql
    base template

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const UserList = [
+    {
+        id: 1,
+        name: "John",
+        username: "john",
+        age: 20,
+        nationality: "Canada",
+    },
+    {
+        id: 2,
+        name: "John2",
+        username: "john2",
+        age: 22,
+        nationality: "Canada2",
+    },
+    {
+        id: 3,
+        name: "John3",
+        username: "john3",
+        age: 23,
+        nationality: "Canada3",
+    },
+    {
+        id: 4,
+        name: "John3",
+        username: "john3",
+        age: 24,
+        nationality: "Canada3",
+    },
+    {
+        id: 5,
+        name: "John3",
+        username: "john3",
+        age: 25,
+        nationality: "Canada3",
+    },
+];
+
+module.exports = { UserList }
