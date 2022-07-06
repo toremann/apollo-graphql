74b5c398f157f5fb248166e0af2c669f42e790d9
toremann <50668920+toremann@users.noreply.github.com>
2022-07-06 23:28:39 +0200

    added open
    graphql server opens on run

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./schema/type-defs');
 const { resolvers } = require('./schema/resolvers')
+const { UserList } = require('./FakeData')
+const open = require('open')
 
 const server = new ApolloServer({ typeDefs, resolvers });
+const amountOfUsers = UserList.length
 
 server.listen().then(({url}) => {
-    console.log(`Server running: ${url}`);
+    open(url)
+    console.log(`Server running: ${url}, currently ${amountOfUsers} fake users in DB`);
 });
