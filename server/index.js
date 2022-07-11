3acfab30163d2fb33d84e2d130ff44492f563a34
toremann <50668920+toremann@users.noreply.github.com>
2022-07-11 17:41:20 +0200

    added logging

--- server/index.js
+++ server/index.js
@@ -1,13 +1,36 @@
 const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./schema/type-defs');
-const { resolvers } = require('./schema/resolvers')
-const { UserList } = require('./FakeData')
-const open = require('open')
+const { resolvers } = require('./schema/resolvers');
+const { UserList } = require('./FakeData');
+const open = require('open');
+const fs = require('fs');
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const amountOfUsers = UserList.length
+// const lastId = logFile[logFile.length - 1].id;
+// const id = lastId + 1;
 
-server.listen().then(({url}) => {
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: (params) => () => {
+        fs.appendFileSync(
+            './logs/logs.json',
+            JSON.stringify(
+                {
+                    date: new Date().toLocaleDateString('en-GB'),
+                    time: new Date().toLocaleTimeString('en-GB'),
+                    ip: params.req.ip,
+                    query: 'some query',
+                },
+                null,
+                4
+            )
+        );
+    },
+});
+
+const amountOfUsers = UserList.length;
+
+server.listen().then(({ url }) => {
     // open(url)
     console.log(`Server running: ${url}, currently ${amountOfUsers} fake users in DB`);
 });
