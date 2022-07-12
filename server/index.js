75b9a9bfe8f0fffae5ca86c8a51fc62c39f7682c
toremann <50668920+toremann@users.noreply.github.com>
2022-07-12 10:42:23 +0200

    logging

--- server/index.js
+++ server/index.js
@@ -12,6 +12,18 @@ const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: (params) => () => {
+        let vars = params.req.body.variables;
+        let query = params.req.body.query;
+
+        console.log('vars type', typeof vars);
+        console.log(Object.keys(vars).length);
+
+        if (Object.keys(vars).lenght === 0) {
+            console.log('no vars');
+        } else {
+            console.log('it has vars', params.req.body.variables);
+        }
+
         fs.appendFileSync(
             './logs/logs.json',
             JSON.stringify(
@@ -19,7 +31,8 @@ const server = new ApolloServer({
                     date: new Date().toLocaleDateString('en-GB'),
                     time: new Date().toLocaleTimeString('en-GB'),
                     ip: params.req.ip,
-                    query: 'some query',
+                    query: query,
+                    variables: vars,
                 },
                 null,
                 4
@@ -32,5 +45,6 @@ const amountOfUsers = UserList.length;
 
 server.listen().then(({ url }) => {
     // open(url)
+
     console.log(`Server running: ${url}, currently ${amountOfUsers} fake users in DB`);
 });
