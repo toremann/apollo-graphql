1c8173e76ed581bbca89ca41a4991cd97ef9a8a5
toremann <50668920+toremann@users.noreply.github.com>
2022-07-14 10:53:41 +0200

    cleanup

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -12,47 +12,10 @@ const resolvers = {
             const user = _.find(UserList, { id: Number(id) }); //shorthand notation
             return user;
         },
-
-        // MOVIE RESOLVERS
-        movies: () => {
-            return MovieList;
-        },
-        movie: (parent, args) => {
-            const name = args.name;
-            const movie = _.find(MovieList, { name }); //shorthand notation
-            return movie;
-        },
     },
     User: {
         favouriteMovies: () => {
             return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010);
-        },
-    },
-
-    Mutation: {
-        createUser: (parent, args) => {
-            const user = args.input;
-            const lastId = UserList[UserList.length - 1].id;
-            user.id = lastId + 1;
-            UserList.push(user);
-            return user;
-        },
-        updateUsername: (parent, args) => {
-            const { id, newUsername } = args.input;
-            let userUpdated;
-            UserList.forEach((user) => {
-                if (user.id === Number(id)) {
-                    user.username = newUsername;
-                    userUpdated = user;
-                }
-            });
-
-            return userUpdated;
-        },
-        deleteUser: (parent, args) => {
-            const id = args.id;
-            _.remove(UserList, (user) => user.id === Number(id));
-            return null, console.log(`deleted user with id: ${id}`);
         },
     },
 };
